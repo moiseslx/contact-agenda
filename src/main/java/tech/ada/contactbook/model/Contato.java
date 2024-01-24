6e38239f07c24a70578f0dec158d35a19c598f82
Moises Almeida <moisesalmeida.dev@gmail.com>
2024-01-23 21:54:37 -0400

    Criado as classes Contato e Telefone

---
+++ src/main/java/tech/ada/contactbook/model/Contato.java
@@ -0,0 +1,10 @@
+package tech.ada.contactbook.model;
+
+import java.util.List;
+
+public class Contato {
+    private Long id;
+    private String nome;
+    private String sobreNome;
+    private List<Telefone> telefones;
+}
---
+++ src/main/java/tech/ada/contactbook/model/Telefone.java
@@ -0,0 +1,7 @@
+package tech.ada.contactbook.model;
+
+public class Telefone {
+    private Long id;
+    private String ddd;
+    private Long numero;
+}
