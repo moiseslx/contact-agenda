62386067c174ac938a16b18d0a35c13c00768f3d
Moises Almeida <moisesalmeida.dev@gmail.com>
2024-01-29 17:01:15 -0400

    Métodos(Get, Set, etc) de Telefone e Contato

--- src/main/java/tech/ada/contactbook/model/Contato.java
+++ src/main/java/tech/ada/contactbook/model/Contato.java
@@ -7,4 +7,67 @@ public class Contato {
     private String nome;
     private String sobreNome;
     private List<Telefone> telefones;
+
+    public Contato(String nome, String sobreNome, List<Telefone> telefones) {
+        setTelefones(telefones);
+        setNome(nome);
+        setSobreNome(sobreNome);
+    }
+
+    public Contato() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        if (id == null) {
+            throw new IllegalArgumentException("ID inválido");
+        }
+
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getSobreNome() {
+        return sobreNome;
+    }
+
+    public List<Telefone> getTelefones() {
+        return telefones;
+    }
+
+    public void setNome(String nome) {
+        if (nome == null || nome.isBlank()) {
+            throw new IllegalArgumentException("Nome inválido");
+        }
+
+        this.nome = nome;
+    }
+
+    public void setSobreNome(String sobreNome) {
+        if (sobreNome == null || sobreNome.isBlank()) {
+            throw new IllegalArgumentException("Sobrenome inválido");
+        }
+
+        this.sobreNome = sobreNome;
+    }
+
+    public void setTelefones(List<Telefone> telefones) {
+        if (telefones == null) {
+            throw new IllegalArgumentException("Lista de telefones inválido");
+        }
+
+        this.telefones = telefones;
+    }
+
+    @Override
+    public String toString() {
+        return id + " | " + nome + " " + sobreNome;
+    }
 }
--- src/main/java/tech/ada/contactbook/model/Telefone.java
+++ src/main/java/tech/ada/contactbook/model/Telefone.java
@@ -2,6 +2,76 @@ package tech.ada.contactbook.model;
 
 public class Telefone {
     private Long id;
+
+    private Long idContato;
     private String ddd;
     private Long numero;
+
+    public Telefone(Long id, String ddd, String numero) {
+        this.id = id;
+        setDdd(ddd);
+        setNumero(numero);
+    }
+
+    public Telefone(String ddd, String numero) {
+        setDdd(ddd);
+        setNumero(numero);
+    }
+
+    public Telefone() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getIdContato() {
+        return idContato;
+    }
+
+    public void setIdContato(Long idContato) {
+        this.idContato = idContato;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDdd() {
+        return ddd;
+    }
+
+    public Long getNumero() {
+        return numero;
+    }
+
+    public void setDdd(String ddd) {
+        String REGEX_DDD = "[0-9]{2}";
+        if (!ddd.matches(REGEX_DDD)) {
+            throw new IllegalArgumentException("DDD inválido. DDD deve ter 2 dígitos.");
+        }
+
+        this.ddd = ddd;
+    }
+
+    public void setNumero(String numero) {
+        String REGEX_NUMERO = "[0-9]{9}|[0-9]{8}";
+        if (!numero.matches(REGEX_NUMERO)) {
+            throw new IllegalArgumentException("Número inválido. Número deve ter apenas 8 dígitos numéricos.");
+        }
+
+        this.numero = Long.parseLong(numero);
+    }
+
+    @Override
+    public String toString() {
+        String telefone = null;
+
+        if (numero.toString().length() == 8) {
+            telefone = String.format("+55 %s %s-%s", ddd, numero.toString().substring(0, 4), numero.toString().substring(4));
+        }
+
+        return telefone;
+    }
 }
